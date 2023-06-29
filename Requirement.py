e46bcfbb2913769cb950c80554514a92990601ff
paarthsaggarwal <79054391+paarthsaggarwal@users.noreply.github.com>
2023-06-29 19:09:51 +0100

    Update Requirement.py

--- Requirement.py
+++ Requirement.py
@@ -1,9 +1,5 @@
 import speech_recognition as sr
 import wit
-from gtts import gTTS
-from io import BytesIO
-from playsound import playsound
-from googletrans import Translator
 import time
 import RPi.GPIO as GPIO
 from gpiozero import Robot
